38d8ead0e6cbf5b5c1d8bf1a742e4ca05bd0eec5
sejin8839 <sejinon01@gmail.com>
2023-05-15 22:49:38 +0900

    Create Assignment8_P2.cpp

---
+++ Assignment8_P2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+/*
+
+filename: Assignment8_P2.cpp
+Developer Name: ........
+Date:
+
+*/
+
+class Matrix
+{
+    private:
+    int mat[10][10];
+    int rows;
+    int cols;
+    public:
+    void input()
+    {
+         //Students Todos
+    }
+    void output()
+    {
+         //Students Todos
+    }
+    Matrix add(Matrix t)
+    {
+         //Students Todos
+    }
+    Matrix sub(Matrix t)
+    {
+
+        //Students Todos
+    }
+};
+int main()
+{
+Matrix m1,m2,add_mat,sub_mat;
+cout<<"*********Matrix m1*********"<<endl;
+m1.input();
+cout<<"*********Matrix m2*********"<<endl;
+m2.input();
+add_mat=m1.add(m2);
+sub_mat=m1.sub(m2);
+cout<<"Addition of two matrices is"<<endl;
+add_mat.output();
+cout<<"Subtraction of two matrices is"<<endl;
+sub_mat.output();
+
+}
